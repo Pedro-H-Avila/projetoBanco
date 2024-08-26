24253539a596f2d30650e5a69f47462f6fd6f40a
Pedro Vasconcelos <151564394+Pedro-H-Avila@users.noreply.github.com>
2024-08-26 17:01:41 -0300

    Update TestaMetodo.java

--- TestaMetodo.java
+++ TestaMetodo.java
@@ -1,20 +1,20 @@
 public class TestaMetodo { 
     public static void main (String[] args) {
-        Conta contaDoPaulo = new Conta();
-        contaDoPaulo.saldo = 100; 
-        contaDoPaulo.deposita(50);
-        System.out.println(contaDoPaulo.saldo);
+        Conta conta001 = new Conta();
+        conta001.saldo = 100; 
+        conta001.deposita(50);
+        System.out.println(conta001.saldo);
 
-        boolean conseguiuRetirar = contaDoPaulo.saca(20);
-        /*  if(contaDoPaulo.saca(20)){
+        boolean conseguiuRetirar = conta001.saca(20);
+        /*  if(conta001.saca(20)){
          System.out.println("conseguir sacar!");
         } else {
             System.out.println("Não conseguiu sacar!");
         }*/
-        System.out.println(contaDoPaulo.saldo);
+        System.out.println(conta001.saldo);
         System.out.println(conseguiuRetirar);
 
-        Conta contaDaMarcela = new Conta();
-        contaDaMarcela.deposita(1000);
+        Conta conta002 = new Conta();
+        conta002.deposita(1000);
     }
 }
